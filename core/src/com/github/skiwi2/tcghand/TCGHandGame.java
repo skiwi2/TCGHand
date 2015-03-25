75ef0fadc42a67b856c6e5820b3718fc2bedd1ee
skiwi <frank_skiwi@hotmail.com>
2015-03-25 17:03:28 +0100

    Added lighting.

--- core/src/com/github/skiwi2/tcghand/TCGHandGame.java
+++ core/src/com/github/skiwi2/tcghand/TCGHandGame.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class TCGHandGame extends ApplicationAdapter {
@@ -19,6 +21,8 @@ public class TCGHandGame extends ApplicationAdapter {
 	private ModelBatch modelBatch;
 	private Model model;
 	private ModelInstance instance;
+
+	private Environment environment;
 
 	@Override
 	public void create () {
@@ -36,6 +40,10 @@ public class TCGHandGame extends ApplicationAdapter {
 			new Material(ColorAttribute.createDiffuse(Color.RED)),
 			Usage.Position | Usage.Normal);
 		instance = new ModelInstance(model);
+
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 	}
 
 	@Override
@@ -47,7 +55,7 @@ public class TCGHandGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		modelBatch.begin(camera);
-		modelBatch.render(instance);
+		modelBatch.render(instance, environment);
 		modelBatch.end();
 	}
 
