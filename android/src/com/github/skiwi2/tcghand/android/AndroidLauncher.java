25d498dcd06919e3688d2316a9b9092f0cd0a44c
skiwi <frank_skiwi@hotmail.com>
2015-03-25 13:41:13 +0100

    Initial commit.

---
+++ android/src/com/github/skiwi2/tcghand/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.skiwi2.tcghand.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.skiwi2.tcghand.TCGHandGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TCGHandGame(), config);
+	}
+}
---
+++ desktop/src/com/github/skiwi2/tcghand/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.skiwi2.tcghand.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.skiwi2.tcghand.TCGHandGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TCGHandGame(), config);
+	}
+}
